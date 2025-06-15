fcbac825f946ef2c514b80433433a2f9fff75695
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-06-15 20:21:20 +0900

    Add: IF_UNIT.php | PHPDoc

---
+++ IF_UNIT.php
@@ -0,0 +1,8 @@
+<?php
+/**	op-core-interface:/IF_UNIT.php
+ *
+ * @created    2019-02-20  op-core-7:/IF_UNIT.php
+ * @moved      2025-06-25  op-core-interface:/IF_UNIT.php
+ * @author     Tomoaki Nagahara
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
