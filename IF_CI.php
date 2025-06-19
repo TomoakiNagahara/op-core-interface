88bdc1303bac25840725ee2d490b3ccce9134ddf
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-06-19 15:53:39 +0900

    Add: IF_CI.php | PHPDoc

---
+++ IF_CI.php
@@ -0,0 +1,10 @@
+<?php
+/**	op-core-interface:/IF_CI.php
+ *
+ * @created    2024-11-24
+ * @version    1.0
+ * @package    op-core
+ * @subpackage interface
+ * @author     Tomoaki Nagahara
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
