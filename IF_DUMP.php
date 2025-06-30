79bf0f77db59911e51cae91b21c226ca7229a61c
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-06-30 14:47:38 +0900

    NEW: IF_DUMP.php

---
+++ IF_DUMP.php
@@ -0,0 +1,27 @@
+<?php
+/**	op-core-interface:/IF_DUMP.php
+ *
+ * @created    2025-06-19
+ * @subpackage interface
+ * @author     Tomoaki Nagahara
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+
+/**	namespace
+ *
+ */
+namespace OP;
+
+/**	IF_DUMP
+ *
+ * @created    2025-06-19
+ * @version    1.0
+ * @package    op-core
+ * @subpackage interface
+ * @author     Tomoaki Nagahara
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+interface IF_DUMP extends IF_UNIT
+{
+
+}
