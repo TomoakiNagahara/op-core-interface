3bb886c84d6c8b0a796b0f8f602b0e3a7f2bf6cf
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-06-30 14:46:22 +0900

    NEW: IF_API.php

---
+++ IF_API.php
@@ -0,0 +1,69 @@
+<?php
+/**	op-core:/interface/IF_API.php
+ *
+ * @created    2024-02-09
+ * @author     Tomoaki Nagahara
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+
+/**	Declare strict
+ *
+ */
+declare(strict_types=1);
+
+/**	namespace
+ *
+ */
+namespace OP;
+
+/**	IF_API
+ *
+ * @created    2024-02-09
+ * @version    1.0
+ * @package    op-core
+ * @subpackage interface
+ * @author     Tomoaki Nagahara
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+interface IF_API extends IF_UNIT
+{
+	/**	Register for admin information.
+	 *
+	 * <pre>
+	 * OP()->Unit()->Api()->Admin('endpoint', __FILE__);
+	 * </pre>
+	 *
+	 * @created    2024-02-09
+	 */
+	static public function Admin(string $key, $value);
+
+	/**	Register error message for end user.
+	 *
+	 * <pre>
+	 * OP()->Unit()->Api()->Error("This is error message.");
+	 * </pre>
+	 *
+	 * @created    2024-02-09
+	 */
+	static public function Error(string $message);
+
+	/**	Register result.
+	 *
+	 * <pre>
+	 * OP()->Unit()->Api()->Result(['color'=>'blue']);
+	 * </pre>
+	 *
+	 * @created    2024-02-09
+	 */
+	static public function Result($value);
+
+	/**	Output JSON.
+	 *
+	 * <pre>
+	 * OP()->Unit()->Api()->Out();
+	 * </pre>
+	 *
+	 * @created    2024-02-09
+	 */
+	static public function Out();
+}
