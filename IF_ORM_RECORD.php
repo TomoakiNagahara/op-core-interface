15c666476838ad04ad400e6937596311e323db1b
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-06-30 14:48:32 +0900

    NEW: IF_ORM_RECORD.php

---
+++ IF_ORM_RECORD.php
@@ -0,0 +1,48 @@
+<?php
+/**	op-core-interface:/IF_ORM_RECORD.php
+ *
+ * @created    2018-06-29
+ * @author     Tomoaki Nagahara
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+
+/**	namespace
+ *
+ */
+namespace OP;
+
+/**	IF_ORM_RECORD
+ *
+ * @created    2018-06-29
+ * @version    1.0
+ * @package    op-core
+ * @subpackage interface
+ * @author     Tomoaki Nagahara
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+interface IF_ORM_RECORD
+{
+	/**	Is this record matches conditions?
+	 *
+	 *  True: Found saved record.
+	 * False: Empty record. (Do save is create new record.)
+	 *
+	 * @return  boolean  $io
+	 */
+	public function isFind();
+
+	/**	Is this record values was valid by rule?
+	 *
+	 *  True: Can save.
+	 * False: Can not save.
+	 *
+	 * @return  boolean  $io
+	 */
+	public function isValid();
+
+	/**	Return already instantiated Form object. (So-call singleton)
+	 *
+	 * @return  IF_FORM  $form
+	 */
+	public function Form();
+}
