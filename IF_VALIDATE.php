a285282de931dcadce6a9f189a544438fe538efd
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-06-30 14:49:05 +0900

    NEW: IF_VALIDATE.php

---
+++ IF_VALIDATE.php
@@ -0,0 +1,26 @@
+<?php
+/**	op-core-interface:/IF_VALIDATE.php
+ *
+ * @created    2024-06-12
+ * @author     Tomoaki Nagahara
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+
+/**	namespace
+ *
+ */
+namespace OP;
+
+/**	IF_FORM
+ *
+ * @created    2024-06-12
+ * @version    1.0
+ * @package    op-core
+ * @subpackage interface
+ * @author     Tomoaki Nagahara
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+interface IF_VALIDATE
+{
+
+}
