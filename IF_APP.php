a9557ba7ea902ec01b9f4e8e206bc65e982292c9
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-06-30 14:47:13 +0900

    NEW: IF_APP.php

---
+++ IF_APP.php
@@ -0,0 +1,30 @@
+<?php
+/**	op-core-interface:/IF_APP.php
+ *
+ * @created    2019-02-20
+ * @author     Tomoaki Nagahara
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+
+/**	namespace
+ *
+ */
+namespace OP;
+
+/**	IF_APP
+ *
+ * @created    2019-02-20
+ * @version    1.0
+ * @package    op-core
+ * @subpackage interface
+ * @author     Tomoaki Nagahara
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+interface IF_APP extends IF_UNIT
+{
+	/**	Output content.
+	 *
+	 * @created    2024-07-09
+	 */
+	static public function Content();
+}
