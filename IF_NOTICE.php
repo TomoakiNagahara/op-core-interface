9df1014c16c5ec779d7bedc6bfd7f40eb6715346
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-06-30 14:48:10 +0900

    NEW: IF_NOTICE.php

---
+++ IF_NOTICE.php
@@ -0,0 +1,25 @@
+<?php
+/**	op-core-interface:/IF_NOTICE.php
+ *
+ * @created    2025-06-16
+ * @author     Tomoaki Nagahara
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+
+/**	namespace
+ *
+ */
+namespace OP;
+
+/**	IF_NOTICE
+ *
+ * @created    2025-06-16
+ * @version    1.0
+ * @package    op-core
+ * @subpackage interface
+ * @author     Tomoaki Nagahara
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+interface IF_NOTICE extends IF_UNIT
+{
+}
