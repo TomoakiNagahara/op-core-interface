e2527f451144ebedf78779e405e397b11929df9c
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-07-05 18:25:05 +0900

    New: IF_LAYOUT :: Name( string $name=null ) : string

--- IF_LAYOUT.php
+++ IF_LAYOUT.php
@@ -22,4 +22,25 @@ namespace OP;
  */
 interface IF_LAYOUT extends IF_UNIT
 {
+	/**	Set / Get layout name.
+	 *
+	 * Set the layout name to choose which layout to apply.
+	 * This layout name corresponds to the directory name of the layout package.
+	 * All layouts should be stored under the "asset/layout" directory.
+	 *
+	 * The returned layout name is the name of the currently set layout.
+	 *
+	 * <pre>
+	 * //  Set
+	 * IF_LAYOUT::Name('layout_name');
+	 *
+	 * //  Get
+	 * $layout_name = IF_LAYOUT::Name();
+	 * </pre>
+	 *
+	 * @created    2025-07-05
+	 * @param      string     $layout_name
+	 * @return     string     $layout_name
+	 */
+	static function Name( string $name=null ) : string;
 }
