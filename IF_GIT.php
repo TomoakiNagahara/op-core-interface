c82d7776b64fc894c3a40b1e2c66bfb6849d6981
TomoakiMacBookAirM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-07-06 22:46:41 +0900

    NEW: IF_GIT.php

---
+++ IF_GIT.php
@@ -0,0 +1,25 @@
+<?php
+/**	op-core-interface:/IF_GIT.php
+ *
+ * @created    2025-07-06
+ * @author     Tomoaki Nagahara
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+
+/**	namespace
+ *
+ */
+namespace OP;
+
+/**	IF_CD
+ *
+ * @created    2025-07-05
+ * @version    1.0
+ * @package    op-core
+ * @subpackage interface
+ * @author     Tomoaki Nagahara
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+interface IF_GIT extends IF_UNIT
+{
+}
