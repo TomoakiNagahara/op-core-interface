95b5c9721a8998fd2016c361d47b8c1ec024f99d
TomoakiMacBookProM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-11-30 04:20:27 +0900

    Doc: IF_ORM_RECORD.php | PHPDoc

--- IF_ORM_RECORD.php
+++ IF_ORM_RECORD.php
@@ -2,21 +2,25 @@
 /**	op-core-interface:/IF_ORM_RECORD.php
  *
  * @created    2018-06-29
- * @version    1.0
+ * @porting    2025-11-30  from op-core-7:/interface/IF_ORM_RECORD.php
  * @package    op-core
  * @subpackage interface
- * @author     Tomoaki Nagahara
- * @copyright  Tomoaki Nagahara All right reserved.
+ * @copyright  2018 Tomoaki Nagahara All Rights Reserved.
  */
 
-/**	namespace
+/**	Declare strict type
+ *
+ */
+declare(strict_types=1);
+
+/**	Namespace
  *
  */
 namespace OP;
 
 /**	IF_ORM_RECORD
  *
- * @created    2018-06-29
+ * @created   2018-06-29
  */
 interface IF_ORM_RECORD
 {
@@ -38,7 +42,7 @@ interface IF_ORM_RECORD
 	 */
 	public function isValid();
 
-	/**	Return already instantiated Form object. (So-call singleton)
+	/**	Return already instantiated Form object.
 	 *
 	 * @return  IF_FORM  $form
 	 */
