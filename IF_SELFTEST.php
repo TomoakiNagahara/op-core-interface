6f551f0122aaa1c5cb14d2a2c088673c1b1f1637
TomoakiMacBookProM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-11-29 14:03:00 +0900

    New: IF_SELFTEST.php

---
+++ IF_SELFTEST.php
@@ -0,0 +1,23 @@
+<?php
+/**	op-core-interface:/IF_SELFTEST.php
+ *
+ * @created    2025-11-29
+ * @version    1.0
+ * @package    op-core
+ * @subpackage interface
+ * @author     Tomoaki Nagahara
+ * @copyright  Tomoaki Nagahara All rights reserved.
+ */
+
+/**	namespace
+ *
+ */
+namespace OP;
+
+/**	IF_CD
+ *
+ * @created    2025-11-29
+ */
+interface IF_SELFTEST extends IF_UNIT
+{
+}
