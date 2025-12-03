93daa4561c91345c77a3b5ff791f551e2ccb2512
TomoakiMacBookProM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-12-03 18:16:33 +0900

    New: IF_SELFTEST_INSPECTOR.php

---
+++ IF_SELFTEST_INSPECTOR.php
@@ -0,0 +1,27 @@
+<?php
+/**	op-core-interface:/IF_SELFTEST_INSPECTOR.php
+ *
+ * @created    2025-12-03
+ * @version    1.0
+ * @package    op-core
+ * @subpackage interface
+ * @copyright  2025 Tomoaki Nagahara All Rights Reserved.
+ */
+
+/**	Declare strict type
+ *
+ */
+declare(strict_types=1);
+
+/**	Namespace
+ *
+ */
+namespace OP;
+
+/**	IF_CD
+ *
+ * @created    2025-12-03
+ */
+interface IF_SELFTEST_INSPECTOR extends IF_UNIT
+{
+}
