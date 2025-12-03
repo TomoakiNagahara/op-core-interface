deaff413c073a620aee2e36431e4a77409bfe193
TomoakiMacBookProM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2025-12-03 18:10:05 +0900

    New: IF_SELFTEST_CONFIG.php

---
+++ IF_SELFTEST_CONFIG.php
@@ -0,0 +1,27 @@
+<?php
+/**	op-core-interface:/IF_SELFTEST_CONFIG.php
+ *
+ * @created    2025-12-03
+ * @version    1.0
+ * @package    op-core
+ * @subpackage interface
+ * @copyright  2025 Tomoaki Nagahara All Rights Reserved.
+ */
+
+/**	Declare strict type
+ *
+ */
+declare(strict_types=1);
+
+/**	Namespace
+ *
+ */
+namespace OP;
+
+/**	IF_CD
+ *
+ * @created    2025-12-03
+ */
+interface IF_SELFTEST_CONFIG extends IF_UNIT
+{
+}
