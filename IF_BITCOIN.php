53c11f5691da22a19822392a3c09e8faa95c3cc8
TomoakiMacBookProM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2026-01-19 20:38:53 +0900

    New: IF_BITCOIN.php

---
+++ IF_BITCOIN.php
@@ -0,0 +1,26 @@
+<?php
+/**	op-core-interface:/IF_BITCOIN.php
+ *
+ * @created    2026-01-19
+ * @license    Apache-2.0
+ * @package    op-core
+ * @subpackage interface
+ * @copyright  Tomoaki Nagahara
+ */
+
+/**	Declare strict type
+ *
+ */
+declare(strict_types=1);
+
+/**	Namespace
+ *
+ */
+namespace OP;
+
+/**	IF_BITCOIN
+ *
+ */
+interface IF_BITCOIN extends IF_UNIT
+{
+}
