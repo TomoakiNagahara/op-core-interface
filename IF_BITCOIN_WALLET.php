59175a515dcc899035a2e4192e14402b13a1ce51
TomoakiMacBookProM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2026-01-19 20:39:25 +0900

    New: IF_BITCOIN_WALLET.php

---
+++ IF_BITCOIN_WALLET.php
@@ -0,0 +1,26 @@
+<?php
+/**	op-core-interface:/IF_BITCOIN_WALLET.php
+ *
+ * @created    2026-01-19
+ * @license    Apache-2.0
+ * @package    op-core
+ * @subpackage interface
+ * @copyright  Tomoaki Nagahara
+ */
+
+/**	Declare strict type
+ *
+ */
+declare(strict_types=1);
+
+/**	Namespace
+ *
+ */
+namespace OP;
+
+/**	IF_BITCOIN_WALLET
+ *
+ */
+interface IF_BITCOIN_WALLET extends IF_UNIT
+{
+}
