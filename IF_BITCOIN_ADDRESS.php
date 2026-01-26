f6d0d55cd11c63f2165b5e509b884085488fa6e9
TomoakiMacBookProM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2026-01-26 18:38:59 +0900

    New: IF_BITCOIN_ADDRESS.php

---
+++ IF_BITCOIN_ADDRESS.php
@@ -0,0 +1,26 @@
+<?php
+/**	op-core-interface:/IF_BITCOIN_ADDRESS.php
+ *
+ * @created    2026-01-26
+ * @license    Apache-2.0
+ * @package    op-core
+ * @subpackage interface
+ * @copyright  Tomoaki Nagahara
+ */
+
+/**	Declare strict type
+ *
+ */
+declare(strict_types=1);
+
+/**	Namespace
+ *
+ */
+namespace OP;
+
+/**	IF_BITCOIN
+ *
+ */
+interface IF_BITCOIN_ADDRESS extends IF_UNIT
+{
+}
