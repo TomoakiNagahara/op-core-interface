4b766a8fe0c69c4e21a8648c0379ead58e15337c
TomoakiMacBookProM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2026-03-13 15:15:39 +0900

    New: IF_BITCOIN_ADDRESS :: Recieve( string $wallet, string $address, int $minconf=1 ) : float | false

--- IF_BITCOIN_ADDRESS.php
+++ IF_BITCOIN_ADDRESS.php
@@ -31,4 +31,16 @@ interface IF_BITCOIN_ADDRESS extends IF_UNIT
 	 * @return     string|false
 	 */
 	public static function Get( string $wallet, string $label ) : string | false ;
+
+	/**	Get the total amount sent for each bitcoin address.
+	 *
+	 * This is the total amount received, not the current balance.
+	 *
+	 * @created    2026-03-09
+	 * @param      string     $wallet
+	 * @param      string     $address
+	 * @param      int        $minconf
+	 * @return     float|false
+	 */
+	public static function Recieve( string $wallet, string $address, int $minconf=1 ) : float | false ;
 }
