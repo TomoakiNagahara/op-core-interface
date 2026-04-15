24089e4164b94e95af388ffbcbf129df2468ff42
TomoakiMacBookProM1 <1668339+TomoakiNagahara@users.noreply.github.com>
2026-04-15 14:10:46 +0900

    New: IF_FTP.php

---
+++ IF_FTP.php
@@ -0,0 +1,27 @@
+<?php
+/**	op-core:/interface/IF_FTP.php
+ *
+ * @created    2026-04-18
+ * @license    Apache-2.0
+ * @package    op-core
+ * @subpackage interface
+ * @copyright  Tomoaki Nagahara
+ */
+
+/**	Declare strict type
+ *
+ */
+declare(strict_types=1);
+
+/**	Namespace
+ *
+ */
+namespace OP;
+
+/**	IF_FTP
+ *
+ * @created    2026-04-18
+ */
+interface IF_FTP extends IF_UNIT
+{
+}
